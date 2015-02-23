7f3c0c64ee947ba37242cac7ddd8c10ad78a1813
Lyon Hill <lyondhill@gmail.com>
2015-02-23 15:51:34 -0700

    remove

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"os"
 )
 
 //
 func main() {
-	fmt.Println("0")
 	handle(ClusterStart())
-	fmt.Println("1")
 	handle(StatusStart())
-	fmt.Println("2")
 	handle(DecisionStart())
-	fmt.Println("3")
 	handle(ActionStart())
-	fmt.Println("4")
 	// do some sleep thing
 }
 
