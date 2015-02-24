d6531e06e114cad4dc84e6b9808383876dc7b280
sdomino <sdomino@pagodabox.com>
2015-02-23 17:09:26 -0700

    updating status logging

--- status.go
+++ status.go
@@ -31,12 +31,12 @@ var(
 //
 func StatusStart() error {
 
-	log.Info("[YOKE - StatusStart] ...\n")
+	log.Info("[STATUS - StatusStart] ...\n")
 
 	//
 	status = &Status{CRole: conf.Role, DBRole: "initialized", State: "booting", UpdatedAt: time.Now()}
 
-	log.Info("[YOKE] Creating Status %+v\n", status)
+	log.Debug("[STATUS] Created status: %+v\n", status)
 
 	//
 	store = scribble.New("./status", log)
@@ -48,12 +48,12 @@ func StatusStart() error {
 	//
 	rpc.Register(status)
 
-	log.Info("[YOKE] Starting RPC server...\n")
+	log.Info("[STATUS] Starting RPC server...\n")
 
 	// RPC SERVER
 	l, err := net.Listen("tcp", ":" + strconv.FormatInt(int64(conf.ClusterPort+1), 10))
 	if err != nil {
-		log.Error("[YOKE] Unable to start server! %v\n", err)
+		log.Error("[STATUS] Unable to start server! %v\n", err)
 		return err
 	}
 
@@ -61,9 +61,9 @@ func StatusStart() error {
 	go func(l net.Listener) {
 		for {
 			if conn, err := l.Accept(); err != nil {
-				log.Error("[YOKE] RPC server - failed to accept connection\n", err.Error())
+				log.Error("[STATUS] RPC server - failed to accept connection\n", err.Error())
 			} else {
-				log.Trace("[YOKE] RPC server - new connection established\n")
+				log.Trace("[STATUS] RPC server - new connection established\n")
 				go rpc.ServeConn(conn)
 			}
 		}
@@ -76,13 +76,13 @@ func StatusStart() error {
 
 //
 func (s *Status) SetDBRole(role string) {
-	log.Info("[YOKE - SetDBRole] setting '%s' on '%s'\n", role, s.CRole)
+	log.Info("[STATUS - SetDBRole] setting role '%s' on node '%s'\n", role, s.CRole)
 
 	s.DBRole = role
 
 	if err := save(s); err != nil {
-		log.Fatal("BONK!", err)
-		panic("Unable to set db role! " + err.Error())
+		log.Fatal("[STATUS - SetDBRole] Failed to save status! %s", err)
+		panic(err)
 	}
 
 	s.UpdatedAt = time.Now()
@@ -90,13 +90,13 @@ func (s *Status) SetDBRole(role string) {
 
 //
 func (s *Status) SetState(state string) {
-	log.Info("[YOKE - SetState] setting '%s' on '%s'\n", state, s.CRole)
+	log.Info("[STATUS - SetState] setting '%s' on '%s'\n", state, s.CRole)
 
 	s.State = state
 
 	if err := save(s); err != nil {
-		log.Fatal("BONK!", err)
-		panic("Unable to set state! " + err.Error())
+		log.Fatal("[STATUS - SetDBRole] Failed to save status! %s", err)
+		panic(err)
 	}
 }
 
@@ -104,8 +104,8 @@ func (s *Status) SetState(state string) {
 
 //
 func Whoami() (*Status, error) {
-	log.Info("[YOKE - Whoami] I am '%s'", list.LocalNode().Name)
-	log.Debug("[YOKE - Whoami] list.LocalNode() - %+v", list.LocalNode())
+	log.Info("[STATUS - Whoami] I am '%s'", list.LocalNode().Name)
+	log.Debug("[STATUS - Whoami] list.LocalNode() - %+v", list.LocalNode())
 
 	s := &Status{}
 
@@ -119,7 +119,7 @@ func Whoami() (*Status, error) {
 
 //
 func Whois(role string) (*Status, error) {
-	log.Info("[YOKE - Whois] Who is '%s'", role)
+	log.Info("[STATUS - Whois] Who is '%s'?", role)
 
 	var conn string
 
@@ -129,19 +129,19 @@ func Whois(role string) (*Status, error) {
 		}
 	}
 
-	log.Debug("[YOKE - Whois] connection - %s", conn)
+	log.Debug("[STATUS - Whois] connection - %s", conn)
 
 	//
 	client, err := rpc.Dial("tcp", conn)
 	if err != nil {
-		log.Error("[YOKE - Whois] RPC Client unable to dial! %s", err.Error())
+		log.Error("[STATUS - Whois] RPC Client unable to dial! %s", err.Error())
 		return nil, err
 	}
 
 	s := &Status{}
 
 	if err := client.Call("Status.Ping", role, s); err != nil {
-		log.Error("[YOKE - Whois] RPC Client unable to call! %s", err.Error())
+		log.Error("[STATUS - Whois] RPC Client unable to call! %s", err.Error())
 		return nil, err
 	}
 
@@ -153,7 +153,7 @@ func Whois(role string) (*Status, error) {
 
 //
 func Cluster() ([]*Status, error) {
-	log.Info("[YOKE - Cluster]")
+	// log.Info("[STATUS - Cluster]")
 
 	var members = []*Status{}
 
@@ -170,7 +170,7 @@ func Cluster() ([]*Status, error) {
 		members = append(members, s)
 	}
 
-	log.Info("[YOKE - Cluster] members - %+v", members)
+	log.Info("[STATUS - Cluster] members - %+v", members)
 
 	return members, nil
 }
@@ -180,13 +180,13 @@ func Cluster() ([]*Status, error) {
 //
 func (s *Status) Ping(role string, status *Status) error {
 
-	log.Info("[YOKE - Ping] pinging '%s'", role)
+	log.Info("[STATUS - Ping] pinging '%s'...", role)
 
 	//
 	for _, m := range list.Members() {
 		if m.Name == role {
 			if err := get(role, status); err != nil {
-				log.Error("[YOKE - Ping] Unable to read '%s'! %s", role, err.Error())
+				log.Error("[STATUS - Ping] Unable to read '%s'! %s", role, err.Error())
 				return err
 			}
 		}
@@ -197,7 +197,7 @@ func (s *Status) Ping(role string, status *Status) error {
 
 //
 func (s *Status) Demote(source string, status *Status) error {
-	log.Info("[YOKE - Demote] Advising demote...")
+	log.Info("[STATUS - Demote] Advising demote...")
 
 	advice <- "demote"
 
@@ -208,7 +208,7 @@ func (s *Status) Demote(source string, status *Status) error {
 
 //
 func get(role string, status *Status) error {
-	log.Info("[YOKE - get] Attempting to get '%s'", role)
+	log.Debug("[STATUS - get] Attempting to get node '%s'", role)
 
 	t := scribble.Transaction{Operation: "read", Collection: "cluster", RecordID: role, Container: &status}
 	if err := store.Transact(t); err != nil {
@@ -220,7 +220,7 @@ func get(role string, status *Status) error {
 
 //
 func save(status *Status) error {
-	log.Info("[YOKE - get] Attempting to save '%s'", status.CRole)
+	log.Debug("[STATUS - get] Attempting to save node '%s'", status.CRole)
 
 	t := scribble.Transaction{Operation: "write", Collection: "cluster", RecordID: status.CRole, Container: &status}
 	if err := store.Transact(t); err != nil {
