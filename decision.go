3a9b904e4f55803cb1fc5bed8a4d06f9364f3ee8
Lyon Hill <lyondhill@gmail.com>
2015-03-02 16:31:10 -0700

    more data

--- decision.go
+++ decision.go
@@ -149,7 +149,7 @@ func clusterChanges() bool {
 			return true
 		}
 		if member.DBRole != other.DBRole {
-			log.Debug("[decision] The cluster members role changed from %s to %s", member.DBRole, other.DBRole)
+			log.Debug("[decision] The cluster members(%s) role changed from %s to %s", member.CRole, member.DBRole, other.DBRole)
 			lastKnownCluster, _ = Cluster()
 			return true
 		}
